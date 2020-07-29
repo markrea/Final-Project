29cbb6d907dc0a4140ffde009f7593c62000840f
Aidan Mueller <Aidan_Mueller@hotmail.co.uk>
2020-07-29 22:10:39 +0100

    test added fro react router, won't pass yet

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -2,6 +2,10 @@ import React from "react";
 import { render } from "@testing-library/react";
 import App from "../components/App";
 import "@testing-library/jest-dom/extend-expect";
+import { MemoryRouter } from "react-router-dom";
+import Enzyme, { mount } from "enzyme";
+
+Enzyme.configure({ adapter: new Adapter() });
 
 describe("App", () => {
   it("component renders correctly", () => {
@@ -17,5 +21,16 @@ describe("App", () => {
     const { getByTestId } = render(<App />);
     const div = getByTestId("search-div");
     expect(div).toBeInTheDocument();
+  });
+});
+
+describe("with router", () => {
+  it("should load HomePage component", () => {
+    const component = mount(
+      <MemoryRouter initialentries="{['/']}">
+        <App />
+      </MemoryRouter>
+    );
+    expect(component.find(HomePage)).toHaveLength(1)
   });
 });
