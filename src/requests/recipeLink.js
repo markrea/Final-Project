5637b7d5938d5f67e5e07c4aefefbefa6487f879
Mark Rea <markrea366@gmail.com>
2020-08-01 16:55:18 +0100

    fixed package-lock.json issue

--- src/requests/recipeLink.js
+++ src/requests/recipeLink.js
@@ -1,27 +1,27 @@
-import React from "react";
-import axios from "axios";
-import recipeResponseData from "./exampleRecipeData";
+// import React from "react";
+// import axios from "axios";
+// import recipeResponseData from "./exampleRecipeData";
 
-const API_KEY = process.env.REACT_APP_API_KEY1;
-const API_KEY2 = process.env.REACT_APP_API_KEY2;
-const API_KEY3 = process.env.REACT_APP_API_KEY3;
-
-
-
-const recipeLink = async (id) => {
-  await axios
-    .get(
-      `https://api.spoonacular.com/recipes/${id}/information?apiKey=${API_KEY2}`
-    )
-    .then((res) => {
-      console.log("URLS from recipeLink", res.data.sourceUrl);
-      const link = res.data.sourceUrl;
-      return link;
-    })
-    .catch((err) => {
-      alert("Error");
-    });
-};
+// const API_KEY = process.env.REACT_APP_API_KEY1;
+// const API_KEY2 = process.env.REACT_APP_API_KEY2;
+// const API_KEY3 = process.env.REACT_APP_API_KEY3;
 
 
-export default recipeLink;
+
+// const recipeLink = async (id) => {
+//   await axios
+//     .get(
+//       `https://api.spoonacular.com/recipes/${id}/information?apiKey=${API_KEY2}`
+//     )
+//     .then((res) => {
+//       console.log("URLS from recipeLink", res.data.sourceUrl);
+//       const link = res.data.sourceUrl;
+//       return link;
+//     })
+//     .catch((err) => {
+//       alert("Error");
+//     });
+// };
+
+
+// export default recipeLink;
