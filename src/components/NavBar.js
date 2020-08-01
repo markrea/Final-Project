3ddadc916f2c4d50fe06319f6514d20cc607041d
SanMasood <sanamasood.giki@gmail.com>
2020-08-01 13:38:33 +0100

    Rendering navBar

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -3,6 +3,9 @@ import React from "react";
 const NavBar = () => {
     return (
         <div className="navBar">
+            <header className="App-header">
+        <h1>Fridge Tracker</h1>
+      </header>
 
         </div>
     )
