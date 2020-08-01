d368743d14005734fb378d382d8ec8b68ecbb214
SanMasood <sanamasood.giki@gmail.com>
2020-08-01 10:52:11 +0100

    page redirect

--- src/components/SearchResults.js
+++ src/components/SearchResults.js
@@ -1,11 +1,26 @@
 import React from "react";
+import axios from "axios";
+
 import PropTypes from "prop-types";
 
 import "../styles/SearchResults.css";
 import recipeLink from "../requests/recipeLink";
 
+const API_KEY2 = "48aba1bc5bce4ba3901fc4b1e3591949";
 
 const SearchResults = ({ searchResults }) => {
+  const recipeFinder = (id) => {
+    return axios
+      .get(
+        `https://api.spoonacular.com/recipes/${id}/information?apiKey=${API_KEY2}`
+      )
+      .then((response) => {
+        const links = response.data.sourceUrl;
+        console.log(links);
+        return links;
+      })
+      .catch((err) => console.log(err));
+  };
   return (
     <div className="SearchResults" data-testid="results-div">
       <div className="test">
@@ -15,8 +30,15 @@ const SearchResults = ({ searchResults }) => {
               <img src={results.image} alt="mockAlt" className="card-photo" />
 
               <figcaption className="caption">{results.title}</figcaption>
-              <a href={recipeLink(results.id)}>More Details</a>
-              {console.log(recipeLink(results.id))}
+              <a href={recipeFinder(results.id)} target="_blank">
+                More Details
+          </a>
+              {/*<a href="#">
+                <button type="submit" onClick={recipeFinder(results.id)}>
+                  See More
+                </button>
+        </a>*/}
+              {/* console.log(recipeLink(results.id)) */}
             </div>
           );
         })}
