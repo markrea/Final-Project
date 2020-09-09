cfc977743fb805bab72f3447252d3da5bb2d510b
SanMasood <sanamasood.giki@gmail.com>
2020-09-09 23:04:07 +0100

    Search results screenshots

--- src/components/SearchByIngredients/SearchResults.js
+++ src/components/SearchByIngredients/SearchResults.js
@@ -6,20 +6,27 @@ const Result = ({ results }) => {
   const calories = Math.round(results.calories);
 
   return (
-    
     <div key={results.image} className="innerSearchResult">
       <img src={results.image} alt="mockAlt" className="card-photo" />
-      <figcaption className="caption"><b>{results.title}</b></figcaption>
       <figcaption className="caption">
-        Ready in {results.readyInMinutes}mins
+        <b>{results.title}</b>
       </figcaption>
-      <figcaption className="caption">Serves {results.serves}</figcaption>
-      <figcaption className="caption"> {calories} Calories</figcaption>
+      <figcaption className="caption">
+        Ready in {results.readyInMinutes}
+        mins
+      </figcaption>
+      <figcaption className="caption">
+        Serves
+        {results.serves}
+      </figcaption>
+      <figcaption className="caption"> {calories}
+{' '}
+Calories
+</figcaption>
       <a href={results.link} target="_blank" rel="noopener noreferrer">
         View Recipe
       </a>
     </div>
-  
   );
 };
 const SearchResults = ({ searchResults }) => {
--- src/components/SearchByRecipe/SearchResults.js
+++ src/components/SearchByRecipe/SearchResults.js
@@ -5,26 +5,27 @@ import "../../styles/SearchResults.css";
 const Result = ({ results }) => {
   const calories = Math.round(results.calories);
   return (
-    
-      <div key={results.image} className="innerSearchResult">
-        <img src={results.image} alt="mockAlt" className="card-photo" />
-        <figcaption className="caption">
-          <b>{results.title}</b>
-        </figcaption>
-        <figcaption className="caption">
-          Ready in {results.readyInMinutes}
-          mins
-        </figcaption>
-        <figcaption className="caption">
-          Serves
-          {results.serves}
-        </figcaption>
-        <figcaption className="caption"> {calories} Calories</figcaption>
-        <a href={results.link} target="_blank" rel="noopener noreferrer">
-          View Recipe
-        </a>
-      </div>
-   
+    <div key={results.image} className="innerSearchResult">
+      <img src={results.image} alt="mockAlt" className="card-photo" />
+      <figcaption className="caption">
+        <b>{results.title}</b>
+      </figcaption>
+      <figcaption className="caption">
+        Ready in {results.readyInMinutes}
+        mins
+      </figcaption>
+      <figcaption className="caption">
+        Serves
+        {results.serves}
+      </figcaption>
+      <figcaption className="caption"> {calories}
+{' '}
+Calories
+</figcaption>
+      <a href={results.link} target="_blank" rel="noopener noreferrer">
+        View Recipe
+      </a>
+    </div>
   );
 };
 const SearchResults = ({ searchResults }) => {
